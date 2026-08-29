f1652516813fecf60c0427f930140dc6c40d4d59
DanielDev <daniel.agudelomo@amigo.du.co>
2026-08-29 10:27:17 -0500

    first commit

---
+++ app/src/main/java/com/danidev/appmovil2/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.danidev.appmovil2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.danidev.appmovil2.ui.theme.Appmovil2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DadoTheme{
+                MoverDados()
+            }
+        }
+    }
+}
+
+@Composable
+fun MoverDados(){
+    DadoConBotonImagen()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DadoConBotonImagen(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center)){
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) { }
+}

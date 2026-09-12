8d8e61a5cbb4a39c4ab7749ca5753921b9495954
DanielDev <daniel.agudelomo@amigo.du.co>
2026-09-12 12:16:14 -0500

    el app se ajusta y funciona 100%

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,7 @@ plugins {
 
 android {
     namespace = "com.danidev.appmovil2"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.danidev.appmovil2"

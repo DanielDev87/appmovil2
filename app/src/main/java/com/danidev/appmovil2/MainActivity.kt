a43d350f7077529c2294a43ae923f8730336b6c7
DanielDev <daniel.agudelomo@amigo.du.co>
2026-09-18 16:00:07 -0500

    HU-002-animacion-dados

--- app/src/main/java/com/danidev/appmovil2/MainActivity.kt
+++ app/src/main/java/com/danidev/appmovil2/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,8 +18,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -52,6 +59,13 @@ fun DadoConBotonImagen(
     onRollClick: () -> Unit,
     modifier: Modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center)
 ){
+    var rotation by remember { mutableFloatStateOf(0f) }
+    val animatedRotation by animateFloatAsState(
+        targetValue = rotation,
+        animationSpec = tween(durationMillis = 500, easing = LinearEasing),
+        label = "DiceRotation"
+    )
+
     val imageResource = when (diceValue) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
@@ -64,9 +78,20 @@ fun DadoConBotonImagen(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(painter = painterResource(imageResource), contentDescription = diceValue.toString())
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = diceValue.toString(),
+            modifier = Modifier.graphicsLayer(
+                rotationZ = animatedRotation,
+                scaleX = if ((animatedRotation % 360f) == 0f) 1f else 1.1f,
+                scaleY = if ((animatedRotation % 360f) == 0f) 1f else 1.1f
+            )
+        )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = onRollClick){
+        Button(onClick = {
+            rotation += 360f
+            onRollClick()
+        }){
             Text(stringResource(R.string.roll))
         }
     }
